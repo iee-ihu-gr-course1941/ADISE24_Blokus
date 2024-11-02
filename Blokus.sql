39bbdd5b6e3a39517aa5f18ee7e9417162c43026
fooula <vivitsil2002@gmail.com>
2024-11-02 12:54:47 +0200

    Create DB

--- Blokus.sql
+++ Blokus.sql
@@ -14,7 +14,7 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- Dumping structure for πίνακας blokus.board
+
 CREATE TABLE IF NOT EXISTS `board` (
   `cell_id` int(11) NOT NULL AUTO_INCREMENT,
   `x` int(11) NOT NULL,
